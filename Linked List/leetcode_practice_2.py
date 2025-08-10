e54c631abd4d7f6379b8dc0a0965802c5755f3d5
Yasser <yasserfedsi34@gmail.com>
2025-08-11 00:01:12 +0200

    Add solution for "Linked List Cycle" problem (LeetCode #141)

---
+++ Linked List/leetcode_practice_2.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+# Problem: Linked List Cycle (LeetCode #141)
+# input: head = [3,2,0,-4], pos = 1
+# output: true
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+    
