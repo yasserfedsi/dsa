1a31d7fab1a424e4a212829a41b57a0b8ff1416b
Yasser <yasserfedsi34@gmail.com>
2025-08-11 00:08:00 +0200

    Refactor cycle detection initialization in hasCycle method

--- Linked List/leetcode_practice_2.py
+++ Linked List/leetcode_practice_2.py
@@ -11,8 +11,8 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head
-        fast = head
+        fast, slow = head, head
+        
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
